38ce4f1b3f83d11400bf442cde257cab0c90cbce
Elton <eltonwhybrow@icloud.com>
2023-09-03 07:33:28 +0100

    init

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,6 +9,10 @@ export const PromptCard = ({
     post, handleTagClick, handleEdit, handleDelete
 }) => {
 
+    const router = useRouter();
+    const { data: session } = useSession();
+    const pathname = usePathname();
+
     const [copied, setCopied] = useState("");
 
     const handleCopy = () => {
@@ -37,7 +41,6 @@ export const PromptCard = ({
                         </p>
                     </div>
                 </div>
-                {/* TODO: */}
                 <div
                     className="copy_btn"
                     onClick={handleCopy}
@@ -58,6 +61,19 @@ export const PromptCard = ({
             >
                 #{post.tag}
             </p>
+
+            {session?.user.id === post.creator._id && pathname === '/profile' && (
+                <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+                    <p className="font-inter text-sm green_gradient cursor-pointer"
+                        onClick={handleEdit}>
+                        Edit
+                    </p>
+                    <p className="font-inter text-sm orange_gradient cursor-pointer"
+                        onClick={handleDelete}>
+                        Delete
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
